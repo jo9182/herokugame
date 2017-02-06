01d214b22b7a4de2c2ae585604c22ae66c331522
nazzzzz <nk1721@nyu.edu>
2017-02-06 17:25:00 -0500

    finished

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/7', seven);
+app.use('/seven', seven);
 app.use('/2', two);
 app.use('/5', five);
 app.use('/48', fortyeight);
--- routes/seven.js
+++ routes/seven.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/7', function(req, res, next) {
+router.get('/seven', function(req, res, next) {
   res.send('Correct.  Next question: How many horns does a cow have? ');
 });
 
