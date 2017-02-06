09438c083e5f7f08b13ccebff73e1384dfb2c132
nazzzzz <nk1721@nyu.edu>
2017-02-06 16:25:30 -0500

    back

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
 var express = require('express');
-var http = require('http');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+var index = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
-app.all("*", function(request, response, next) {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  next();
-});
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.get('/', function(req,res)
-{
-    res.end("Route path at base address");
-});
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/test', function(req, res) {
-  res.end("Youre gay");
-});
-
-app.get("*", function(request, response) {
-  response.end("404!");
-});
-
-http.createServer(app).listen(1337);
+app.use('/', index);
+app.use('/users', users);
 
 
 
-
-/*
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -50,4 +49,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-*/
