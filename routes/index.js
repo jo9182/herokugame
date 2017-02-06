69654600fb9cf3efc1b359b3a26193b9537cd567
nazzzzz <nk1721@nyu.edu>
2017-02-06 17:19:34 -0500

    finished

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send("Welcome to the game. Type answers to the questions posed in the url, like so:  ...com/ANSWER  First question: How many colors are there in a rainbow?  (Hint: type the number.)")
+    res.send("Welcome to the game. Type answers to the questions posed in the url, like so:  ...com/ANSWER  First question: How many colors are there in a rainbow?  (Hint: type the number.)");
 }
 });
 
