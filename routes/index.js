538cecdcf3f0ef43a45c8c07b8fffd53ed39e81d
nazzzzz <nk1721@nyu.edu>
2017-02-06 17:21:00 -0500

    finished

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next)) {
     res.send("Welcome to the game. Type answers to the questions posed in the url, like so:  ...com/ANSWER  First question: How many colors are there in a rainbow?  (Hint: type the number.)");
 }
 });
